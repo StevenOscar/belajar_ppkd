937367e76a87742f983f704fb6551e9f78e2afbc
unknown <stevenoscar458@gmail.com>
2025-05-24 09:36:43 +0700

    Tugas 5 Flutter Done

--- day5/flutter_app1/lib/main.dart
+++ day5/flutter_app1/lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter_app1/meet_3/meet_3b.dart';
 import 'package:flutter_app1/meet_3/meet_3c.dart';
 import 'package:flutter_app1/tugas/tugas_3_flutter.dart';
 import 'package:flutter_app1/tugas/tugas_4_flutter.dart';
+import 'package:flutter_app1/tugas/tugas_5_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +34,8 @@ class MyApp extends StatelessWidget {
       // home: TugasSatuFlutter(),
       // home: TugasDuaFlutter(),
       // home: TugasTigaFlutter(),
-      home: TugasEmpatFlutter(),
+      // home: TugasEmpatFlutter(),
+      home: TugasLimaFlutter(),
     );
   }
 }
